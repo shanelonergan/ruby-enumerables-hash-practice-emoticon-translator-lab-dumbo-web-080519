ee1d03d15474b3fe60a6841045bb59a1e7c5ef5e
Shane Lonergan <sptlonergan@gmail.com>
2019-07-30 15:55:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,8 +12,10 @@ def load_library(file)
   return emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, english_emoticon)
+  emoticons = load_library(file)
+  japanese_emoticon = emoticons[:get_emoticon][english_emoticon]
+  return japanese_emoticon
 end
 
 def get_english_meaning
