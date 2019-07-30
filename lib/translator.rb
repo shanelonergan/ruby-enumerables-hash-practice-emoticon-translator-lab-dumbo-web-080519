aa26ee3eeff7ea3813e8540453a3b672771a3a89
Shane Lonergan <sptlonergan@gmail.com>
2019-07-30 16:02:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_japanese_emoticon(file, english_emoticon)
     return japanese_emoticon
   else
     return "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning(file, japanese_emoticon)
